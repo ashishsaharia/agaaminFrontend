866fadd15b21edb91f9fc6a04056494483a03397
ashish <ashishsaharia04@gmail.com>
2024-08-19 08:22:29 +0530

    changed the port back

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: true,
-    port: 80
-  },
   plugins: [react()],
+  base: "/agaaminFrontend/"
 })
